c1e39db23d6e89cc156c2ba758148e674484ac11
Izlom1024 <vbif542737@mail.ru>
2018-12-13 22:39:34 +0300

    Создание функции для высчитывания длинны отрезка через static

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.Point.distance;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
@@ -7,16 +9,24 @@ public class MyFirstProgram {
         hello("user");
         hello("Alexei");
 
+        Point p1 = new Point (2, 6);
+        Point p2 = new Point (7, 13);
+
         Square s = new Square(5);
         System.out.println("Площаль квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle (4, 6);
         System.out.println("Площаль прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
+        Rectangle p = new Rectangle (4, 8);
+        System.out.println("Площаль прямоугольника со стороной " + p.a + " и " + p.b + " = " + p.area());
+
+        double dist = distance(p1, p2);
+
+        System.out.println("Длинна отрезка = " + dist);
     }
 
     public static void hello(String somebody) {
+
         System.out.println("Hello, " + somebody + "!");
     }
-
-
 }
